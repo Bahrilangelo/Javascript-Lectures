9337148c4767faaff1d889be9a6f93621f57ee98
bahrilangelo <bahri.official@protonmail.com>
2023-08-24 19:16:14 +0300

    Day 1

---
+++ Fundamentals/1-varaibles.js
@@ -0,0 +1,10 @@
+var hello = 'Hello World';
+
+console.log(hello);
+
+// Varaible rules
+// Can contain letters, numbers, _, $
+// Can not start with a number
+// Only contain english alpahbet
+// Case sensitive
+// Can't contain space 
